025abfeb41683108a175d0138b0ec41dbe9e9890
AndreasVaage <andrevaa@stud.ntnu.no>
2017-04-27 12:19:49 +0200

    smoothed matlab files

--- problem2/problem2.m
+++ problem2/problem2.m
@@ -32,47 +32,47 @@ x1_0 = pi;                              % Lambda
 x2_0 = 0;                               % r
 x3_0 = 0;                               % p
 x4_0 = 0;                               % p_dot
-x0 = [x1_0 x2_0 x3_0 x4_0]';          % Initial values
+x0 = [x1_0 x2_0 x3_0 x4_0]';    % Initial values
 
 % Time horizon and initialization
-N  = 100;                                % Time horizon for states
-M  = N;                                 % Time horizon for inputs
-z  = zeros(N*mx+M*mu,1);                % Initialize z for the whole horizon
-z0 = z;                                 % Initial value for optimization
+N  = 100;              % Time horizon for states
+M  = N;                % Time horizon for inputs
+z  = zeros(N*mx+M*mu,1);% Initialize z for the whole horizon
+z0 = z;         % Initial value for optimization
 
 % Bounds
-ul 	    = -30*pi/180;                   % Lower bound on control -- u1
-uu 	    = 30*pi/180;                    % Upper bound on control -- u1
+ul 	    = -30*pi/180; % Lower bound on control -- u1
+uu 	    = 30*pi/180;  % Upper bound on control -- u1
 
-xl      = -Inf*ones(mx,1);              % Lower bound on states (no bound)
-xu      = Inf*ones(mx,1);               % Upper bound on states (no bound)
-xl(3)   = ul;                           % Lower bound on state x3
-xu(3)   = uu;                           % Upper bound on state x3
+xl      = -Inf*ones(mx,1);% Lower bound on states (no bound)
+xu      = Inf*ones(mx,1); % Upper bound on states (no bound)
+xl(3)   = ul;         % Lower bound on state x3
+xu(3)   = uu;         % Upper bound on state x3
 
 % Generate constraints on measurements and inputs
-[vlb,vub]       = genbegr2(N,N,xl,xu,ul,uu); % hint: genbegr2
-vlb(N*mx+M*mu)  = 0;                    % We want the last input to be zero
-vub(N*mx+M*mu)  = 0;                    % We want the last input to be zero
+[vlb,vub]       = genbegr2(N,N,xl,xu,ul,uu); 
+vlb(N*mx+M*mu)  = 0; % We want the last input to be zero
+vub(N*mx+M*mu)  = 0; % We want the last input to be zero
 
 % Generate the matrix Q and the vector c (objecitve function weights in the QP problem) 
 Q1 = zeros(mx,mx);
-Q1(1,1) = 0.5;                             % Weight on state x1
-Q1(2,2) = 0;                            % Weight on state x2
-Q1(3,3) = 0;                             % Weight on state x3
-Q1(4,4) = 0;                            % Weight on state x4
-P1 = 1;                                 % Weight on input
-Q = 2*genq2(Q1,P1,N,M,mu);              % Generate Q
-c = zeros(N*mx+M*mu,1);                 % Generate c
+Q1(1,1) = 0.5;              % Weight on state x1
+Q1(2,2) = 0;                % Weight on state x2
+Q1(3,3) = 0;                % Weight on state x3
+Q1(4,4) = 0;                % Weight on state x4
+P1 = 1;                     % Weight on input
+Q = 2*genq2(Q1,P1,N,M,mu);  % Generate Q
+c = zeros(N*mx+M*mu,1);     % Generate c
 
 %% Generate system matrices for linear model
-Aeq = gena2(A1,B1,N,mx,mu);  		 % Generate A, hint: gena2
+Aeq = gena2(A1,B1,N,mx,mu); % Generate A, hint: gena2
 beq = zeros(400,1);
-%beq = zeros(N*mx+M*mu,1);            % Generate b
+%beq = zeros(N*mx+M*mu,1);  % Generate b
 beq(1:mx) = A1*x0; % Initial value
 
 %% Solve QP problem with linear model
 tic
-[z,lambda] = quadprog(Q,c,[],[],Aeq,beq,vlb,vub); % hint: quadprog
+[z,lambda] = quadprog(Q,c,[],[],Aeq,beq,vlb,vub); 
 t1=toc;
 
 % Calculate objective value
@@ -87,10 +87,10 @@ end
 
 u  = [z(N*mx+1:N*mx+M*mu);z(N*mx+M*mu)]; % Control input from solution
 
-x1 = [x0(1);z(1:mx:N*mx)];              % State x1 from solution
-x2 = [x0(2);z(2:mx:N*mx)];              % State x2 from solution
-x3 = [x0(3);z(3:mx:N*mx)];              % State x3 from solution
-x4 = [x0(4);z(4:mx:N*mx)];              % State x4 from solution
+x1 = [x0(1);z(1:mx:N*mx)];  % State x1 from solution
+x2 = [x0(2);z(2:mx:N*mx)];  % State x2 from solution
+x3 = [x0(3);z(3:mx:N*mx)];  % State x3 from solution
+x4 = [x0(4);z(4:mx:N*mx)];  % State x4 from solution
 
 num_variables = 5/delta_t;
 zero_padding = zeros(num_variables,1);
@@ -128,4 +128,10 @@ plot(t,u);
 % xlabel('tid (s)'),ylabel('pdot')
 
 %matlab2tikz('tenQ.tex','parseStrings',true, 'height', '\figureheight', 'width', '\figurewidth');
+
+%% Add time to lambda
+pitchRef = [t', u];
+%plot(pitchRef(1,:), pitchRef(2,:))
+
+%plot(actualTravel2.time, actualTravel2.signals.values);
 
--- problem4/ex4.m
+++ problem4/ex4.m
@@ -25,19 +25,19 @@ mx = size(A,2); % Number of states (number of columns in A)
 mu = size(B,2); % Number of inputs(number of columns in B)
 
 % Initial values
-x1_0 = pi;                              % Lambda
-x2_0 = 0;                               % r
-x3_0 = 0;                               % p
-x4_0 = 0;                               % p_dot
+x1_0 = pi;      % Lambda
+x2_0 = 0;       % r
+x3_0 = 0;       % p
+x4_0 = 0;       % p_dot
 x5_0 = 0;  
 x6_0 = 0;  
-x0 = [x1_0 x2_0 x3_0 x4_0 x5_0 x6_0]';          % Initial values
+x0 = [x1_0 x2_0 x3_0 x4_0 x5_0 x6_0]'; % Initial values
 
 %% Time horizon and initialization
-N  = 40;                                % Time horizon for states
-M  = N;                                 % Time horizon for inputs
-z0  = zeros(N*mx+M*mu,1);                % Initialize z for the whole horizon
-z0(1:mx) = x0;                           % Initial value for optimization
+N  = 40;          % Time horizon for states
+M  = N;           % Time horizon for inputs
+z0  = zeros(N*mx+M*mu,1); % Initialize z for the whole horizon
+z0(1:mx) = x0;    % Initial value for optimization
 
 %% Bounds
 upl = -20 * pi / 180; % Pitch lower
@@ -54,22 +54,22 @@ vlb(length(vlb)-mu*N-mx:length(vlb)-mu*N) = mx*[0];
 
 %% Generate the matrix Q and the vector c (objecitve function weights in the QP problem) 
 Q1 = zeros(mx,mx);
-Q1(1,1) = 0.5;                             % Weight on travel
-Q1(2,2) = 1;                            % Weight on travel dot
-Q1(3,3) = 0;                             % Weight on pitch
-Q1(4,4) = 1;                            % Weight on pitch dot
-Q1(5,5) = 1;                            % Weight on elevation
-Q1(6,6) = 1;                            % Weight on elevation dot
+Q1(1,1) = 0.5;   % Weight on travel
+Q1(2,2) = 1;  % Weight on travel dot
+Q1(3,3) = 0;  % Weight on pitch
+Q1(4,4) = 1;  % Weight on pitch dot
+Q1(5,5) = 1;  % Weight on elevation
+Q1(6,6) = 1;  % Weight on elevation dot
 
-q1 = 0.5;                                 % Weight on input pitch
-q2 = 0.5;                                 % Weight on input elevation
+q1 = 0.5;     % Weight on input pitch
+q2 = 0.5;     % Weight on input elevation
 
-Q = 2*genq2(Q1,[q1, 0; 0, q2],N,M,mu);    % Generate Q
+Q = 2*genq2(Q1,[q1, 0; 0, q2],N,M,mu); % Generate Q
 
 %% Generate system matrices for linear model
-Aeq = gena2(A,B,N,mx,mu);  		 % Generate A, hint: gena2
+Aeq = gena2(A,B,N,mx,mu);  % Generate A
 beq = zeros(size(Aeq,1),1);
-%beq = zeros(N*mx+M*mu,1);            % Generate b
+%beq = zeros(N*mx+M*mu,1); % Generate b
 beq(1:mx) = A*x0; % Initial value
 
 
@@ -87,12 +87,12 @@ z = fmincon(objective, z0,[],[],Aeq,beq,vlb,vub,@nonlcon,options);
 u1  = [z(N*mx+1:2:N*mx+M*mu);z(N*mx+M*mu)]; % Control input from solution
 u2  = [z(N*mx+2:2:N*mx+M*mu);z(N*mx+M*mu)]; % Control input from solution
 
-x1 = [x0(1);z(1:mx:N*mx)];              % State x1 from solution
-x2 = [x0(2);z(2:mx:N*mx)];              % State x2 from solution
-x3 = [x0(3);z(3:mx:N*mx)];              % State x3 from solution
-x4 = [x0(4);z(4:mx:N*mx)];              % State x4 from solution
-x5 = [x0(5);z(5:mx:N*mx)];              % State x5 from solution
-x6 = [x0(6);z(6:mx:N*mx)];              % State x6 from solution
+x1 = [x0(1);z(1:mx:N*mx)];  % State x1 from solution
+x2 = [x0(2);z(2:mx:N*mx)];  % State x2 from solution
+x3 = [x0(3);z(3:mx:N*mx)];  % State x3 from solution
+x4 = [x0(4);z(4:mx:N*mx)];  % State x4 from solution
+x5 = [x0(5);z(5:mx:N*mx)];  % State x5 from solution
+x6 = [x0(6);z(6:mx:N*mx)];  % State x6 from solution
 
 num_variables = 5/delta_t;
 zero_padding = zeros(num_variables,1);
